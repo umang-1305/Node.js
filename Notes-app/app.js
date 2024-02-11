7849bb26c8c8821e4c4a866e8b5ca9a98f833357
umang-1305 <umangcharaya111@gmail.com>
2024-02-11 21:53:27 +0530

    Validator Package

--- Notes-app/app.js
+++ Notes-app/app.js
@@ -1,7 +1,9 @@
+const validator=require('validator')
 const getNotes=require('./notes.js')
 const msg=getNotes()
-console.log
+console.log (msg)
 
+console.log (validator.isEmail('umangcharaya111@gmail.com'))
 //const add = require('./utils.js')
 //const sum=add(2,3)
 //console.log (sum)
